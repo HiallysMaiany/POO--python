79ba7fbc98211cb402c6bc71af75ec62c4f5880b
Hiallys Maiany <hiallys.maiany15@gmail.com>
2024-05-28 20:20:29 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from carro import Carro
+
+fusca = Carro ('Volks', 'Fusca', 'Azul', 'Gasolina')
+
+fusca.ligar()
+fusca.ligar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.acelerar()
+fusca.frear()
+fusca.frear()
+fusca.frear()
+fusca.frear()
+fusca.desligar()
+
+ferrari = Carro('Ferrari', 'Ferrari 911', 'Vermelho', 'Gasolina')
+tesla = Carro('Tesla', 'Cybertruck', 'cinza', 'Elétrico')
+
+ferrari.ligar()
+tesla.ligar()
+    
